8d0dd0ad08648064d9e697b1d63f8de1dc489651
OrkhanKarim13 <mrorkhankarimli@gmail.com>
2023-10-31 12:20:21 +0400

    first commit

---
+++ youplay/src/js/app.js
@@ -0,0 +1,18 @@
+function updateCountdown() {
+    const targetDate = new Date("2024-01-21 16:12:40").getTime();
+    const currentDate = new Date().getTime();
+    const timeDifference = targetDate - currentDate;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = String(days).padStart(3, '0');
+    document.getElementById("hours").textContent = String(hours).padStart(2, '0');
+    document.getElementById("minutes").textContent = String(minutes).padStart(2, '0');
+    document.getElementById("seconds").textContent = String(seconds).padStart(2, '0');
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
